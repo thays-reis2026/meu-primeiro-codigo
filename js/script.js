41b6b35ed23b5493e653168c786ebf3936439216
Thays <thayss2737@gmail.com>
2026-01-07 16:56:35 -0300

    Atualizando alerta de validação do formulário

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ function validaFormulário(){
     if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("N° Telefone").value != ""){
     alert("Prontinho! você receberá as novidades por email.")
     }else{
-        alert("Por favor, preencha os campos nome e email!.")
+        alert("Por favor, preencha os campos nome, email e telefone!.")
     }
 }
