ff29f1dffc441031e696ad0183e5e4c72a791795
Thays <thayss2737@gmail.com>
2026-01-07 16:02:47 -0300

    Meu primeiro código na PrograMaria

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("botãoEnviar").addEventListener("click",validaFormulário)
+
+function validaFormulário(){
+    if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("N° Telefone").value != ""){
+    alert("Prontinho! você receberá as novidades por email.")
+    }else{
+        alert("Por favor, preencha os campos nome e email!.")
+    }
+}
